f3d059fb8c6c37495c7325060117792b62af4777
iurigonzaga-png <iuri.gonzaga@esamcuberlandia.com.br>
2026-08-11 22:09:09 -0300

    Add files via upload

---
+++ Exercicios 6/ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BibliotecaFinanceira
+{
+    public class CalculadoraJuros
+    {
+
+        internal double CalculoInterno(double capital, double taxa, double tempo)
+        {
+            double total = capital * taxa * tempo;
+            return total;
+        }
+        public double JurosSimples(double capital, double taxa, double tempo)
+        {
+            return CalculoInterno(capital, taxa, tempo);
+        }
+    }
+}
---
+++ Exercicios 6/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BibliotecaFinanceira;
+
+namespace ConsoleTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculadoraJuros calculadora = new CalculadoraJuros();
+            double capital = 1000.00;
+            double taxa = 0.05;
+            double tempo = 3;
+            double juros = calculadora.JurosSimples(capital, taxa, tempo);
+            double totalGeral = capital + juros;
+            Console.WriteLine("--- TESTE DA BIBLIOTECA FINANCEIRA ---");
+            Console.WriteLine($"Capital Inicial: {capital:C}");
+            Console.WriteLine($"Taxa de Juros:   {taxa:P}");
+            Console.WriteLine($"Tempo:           {tempo} meses");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine($"Juros Gerados:   {juros:C}");
+            Console.WriteLine($"Total Acumulado: {totalGeral:C}");
+            Console.ReadKey();
+        }
+    }
+}
