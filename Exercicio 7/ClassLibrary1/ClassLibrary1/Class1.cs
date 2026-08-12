33d8ced851c37511d82cf5f07736e1473298332f
iurigonzaga-png <iuri.gonzaga@esamcuberlandia.com.br>
2026-08-11 22:10:11 -0300

    Add files via upload

---
+++ Exercicio 7/ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace BibliotecaValidacoes
+{
+    public class Validador
+    {
+        public bool ValidadorCPF(string cpf)
+        {
+            string apenasNumeros = Regex.Replace(cpf, @"[^\d]", "");
+            return apenasNumeros.Length == 11;
+        }
+        public bool ValidadorEmail(string email)
+        {
+            return Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+        }
+        public bool ValidadorSenha(string senha, int tamanhoMinimo)
+        {
+            return senha.Length >= tamanhoMinimo;
+        }
+    }
+}
---
+++ Exercicio 7/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using BibliotecaValidacoes;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleValidacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Validador validador = new Validador();
+            bool cpfValido = validador.ValidadorCPF("123.456.789-00");
+            bool emailValido = validador.ValidadorEmail("usuario@email.com");
+            bool senhaValida = validador.ValidadorSenha("123456");
+            Console.WriteLine($"CPF Válido? {cpfValido}");
+            Console.WriteLine($"E-mail Válido? {emailValido}");
+            Console.WriteLine($"Senha Válida? {senhaValida}");
+            Console.ReadKey();
+        }
+    }
+}
